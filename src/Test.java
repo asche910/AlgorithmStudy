79add5f4b2b0f946d231de6849e74dd9b70bf87a
asche910 <asche910@163.com>
2020-02-09 18:46:14 +0800

    complete ReversePolishNotation --> from center to back

--- src/Test.java
+++ src/Test.java
@@ -5,43 +5,58 @@ public class Test {
     public static void main(String[] args) throws IOException {
         System.out.println("Test...");
 
-        Map<Short, String> map = new HashMap<>();
-        for (short i = 0; i < 100; i++) {
-            map.put(i, String.valueOf(i));
-            map.remove( "");
-        }
-        System.out.println(map.size());
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        map.put(1, 11);
+        map.put(4, 11);
+        map.put(2, 11);
+
+        System.out.println(Arrays.toString(map.keySet().toArray()));
     }
 
-    public int compress(char[] chars) {
-        if (chars.length == 0) return 0;
-        if (chars.length == 1) return 1;
-        int size = 0;
-        int count = 1;
-        int i = 1;
-        for (; i <= chars.length; i++) {
-            if (i == chars.length || chars[i] != chars[i - 1]) {
-                if (count > 1) {
-                    chars[size++] = chars[i - 1];
-                    System.out.println(count);
-                    if (count < 10) {
-                        chars[size++] = (char) ('0' + count);
-                    } else if (count < 100) {
-                        chars[size++] = (char) ('0' + count / 10);
-                        chars[size++] = (char) ('0' + count % 10);
-                    } else if (count < 1000) {
-                        chars[size++] = (char) ('0' + count / 100);
-                        chars[size++] = (char) ('0' + count % 100 / 10);
-                        chars[size++] = (char) ('0' + count % 10);
-                    }
-                    count = 1;
-                } else {
-                    chars[size++] = chars[i - 1];
-                }
-            } else {
-                count++;
+
+}
+
+class TweetCounts {
+
+    Map<String, TreeMap<Integer, Integer>> map = new HashMap<>();
+
+    public TweetCounts() {
+
+    }
+
+    public void recordTweet(String tweetName, int time) {
+        if (!map.containsKey(tweetName)) {
+            map.put(tweetName, new TreeMap<>());
+        }
+        TreeMap<Integer, Integer> treeMap = map.get(tweetName);
+        treeMap.put(time, treeMap.getOrDefault(time, 0) + 1);
+    }
+
+    public List<Integer> getTweetCountsPerFrequency(String freq, String tweetName, int startTime, int endTime) {
+        TreeMap<Integer, Integer> treeMap = map.get(tweetName);
+        List<Integer> list = new ArrayList<>();
+        Set<Map.Entry<Integer, Integer>> entries = treeMap.entrySet();
+        Iterator<Map.Entry<Integer, Integer>> iterator = entries.iterator();
+        int inter = 0;
+
+        if (freq.equals("minute"))
+            inter = 60;
+        else if (freq.equals("hour"))
+            inter = 3600;
+        else
+            inter = 86400;
+
+        int startN = startTime / inter;
+        int n = endTime / inter - startN;
+        for (int i = 0; i < n + 1; i++) list.add(0);
+
+        while (iterator.hasNext()) {
+            Map.Entry<Integer, Integer> next = iterator.next();
+            Integer key = next.getKey();
+            if (key >= startTime && key <= endTime) {
+                list.set(key / inter - startN, list.get(key / inter - startN) + 1);
             }
         }
-        return size;
+        return list;
     }
 }
---
+++ src/stack/ReversePolishNotation.java
@@ -0,0 +1,75 @@
+package stack;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 逆波兰表达式算法
+ */
+public class ReversePolishNotation {
+    public static void main(String[] args) {
+
+        System.out.println(strToExpression("(6+2*(1+2))*4"));
+    }
+
+    /**
+     * 中缀表达式转化为后缀表达式
+     */
+    public static List<String> strToExpression(String str) {
+        List<String> list = new ArrayList<>();
+        Stack<String> stack = new Stack<>();
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == '+' || ch == '-') {
+                if (stack.isEmpty() || stack.peek().equals("(")) {
+                    stack.push(String.valueOf(ch));
+                } else {
+                    while (!stack.isEmpty()) {
+                        String peek = stack.peek();
+                        if (peek.equals("+") || peek.equals("-") || peek.equals("*") || peek.equals("/")) {
+                            list.add(stack.pop());
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            } else if (ch == '*' || ch == '/') {
+                if (stack.isEmpty()) {
+                    stack.push(String.valueOf(ch));
+                } else {
+                    while (!stack.isEmpty()) {
+                        String peek = stack.peek();
+                        if (peek.equals("*") || peek.equals("/")) {
+                            list.add(stack.pop());
+                        } else {
+                            stack.push(String.valueOf(ch));
+                            break;
+                        }
+                    }
+                }
+            } else if (ch == '(') {
+                stack.push(String.valueOf(ch));
+            } else if (ch == ')') {
+                while (!stack.peek().equals("(")) {
+                    list.add(stack.pop());
+                }
+                stack.pop();
+            } else if(ch == ' '){
+                continue;
+            }else {
+                int len = 1;
+                while (i + len < str.length() && str.charAt(i + len) >= '0' && str.charAt(i + len) <= '9') {
+                    len++;
+                }
+                list.add(str.substring(i, i + len));
+                i += len - 1;
+            }
+        }
+        while (!stack.isEmpty()) {
+            list.add(stack.pop());
+        }
+        return list;
+    }
+}
