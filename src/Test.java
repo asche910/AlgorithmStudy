5b01d2822882e5f53f50ee3e02119574fb682761
asche910 <asche910@163.com>
2020-02-18 12:06:41 +0800

    fix selectionSort error

--- src/Test.java
+++ src/Test.java
@@ -1,43 +1,27 @@
+import tool.Tools;
+
 import java.io.IOException;
 import java.util.*;
 
 public class Test {
     public static void main(String[] args) throws IOException {
         System.out.println("Test...");
-        String str = "abccbcag", pattern = "cbca";
-
-        int[] next = calNext(pattern.toCharArray());
-        int i = 0, j = 0;
-        while(i < str.length() && j < pattern.length()){
-            if(j == -1 || str.charAt(i) == pattern.charAt(j)){
-                i++;
-                j++;
-            }else{
-                j = next[j];
-            }
-        }
-        System.out.println(i + " " + j);
+        int[] nums = new int[]{3, 2, 4, 1, 5, 0};
+        selectionSort(nums);
     }
 
-    public static int[] calNext(char[] chs){
-        int[] next = new int[chs.length];
-        next[0] = -1;
-        int i = -1, j = 0;
-        while(j < chs.length - 1){
-            if(i == -1 || chs[i] == chs[j]){
-                i++;
-                j++;
-                if(chs[i] != chs[j]){
-                    next[j] = i;
-                }else{
-                    next[j] = next[i];
+    public static void selectionSort(int[] nums){
+        for(int i = 0; i < nums.length - 1; i++){
+            int min = i;
+            for(int j = i + 1; j < nums.length; j++){
+                if(nums[j] < nums[min]){
+                    min = j;
                 }
-            }else {
-                i = next[i];
             }
+            Tools.swap(nums, min, i);
         }
-        System.out.println(Arrays.toString(next));
-        return next;
+        System.out.println(Arrays.toString(nums));
     }
+
 }
 
--- src/sort/SelectionSort.java
+++ src/sort/SelectionSort.java
@@ -9,7 +9,7 @@ import java.util.Arrays;
  */
 public class SelectionSort {
     public static void main(String[] args) {
-        int[] nums = new int[]{3, 2, 4, 1, 5, 0};
+        int[] nums = new int[]{-2, 3, 2, 4, 1, 5, 0};
         System.out.println(Arrays.toString(nums));
         selectionSort(nums);
         System.out.println(Arrays.toString(nums));
@@ -17,13 +17,13 @@ public class SelectionSort {
 
     public static void selectionSort(int[] nums){
         for(int i = 0; i < nums.length - 1; i++){
-            int index = i + 1;
+            int min = i;
             for(int j = i + 1; j < nums.length; j++){
-                if (nums[j] < nums[index]){
-                    index = j;
+                if (nums[j] < nums[min]){
+                    min = j;
                 }
             }
-            Tools.swap(nums, i, index);
+            Tools.swap(nums, i, min);
         }
     }
 }
