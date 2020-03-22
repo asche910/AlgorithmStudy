283865cdb1278c3c8564e315c0cd03ea5cfe1b9c
asche910 <asche910@163.com>
2020-03-22 15:26:29 +0800

    add PrimeFactors

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,64 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
 
+    }
+
+    public boolean hasValidPath(int[][] grid) {
+        //  1
+        //4   2
+        //  3
+        if (grid.length == 1 && grid[0].length == 1) return true;
+        int[][] dir = new int[][]{{0, 0}, {4, 2}, {1, 3}, {4, 3}, {2, 3}, {1, 4}, {1, 2}};
+        int[] dirX = new int[]{0, -1, 0, 1, 0};
+        int[] dirY = new int[]{0, 0, 1, 0, -1};
+
+        int i = 0, j = 0, last = 0;
+        if (grid[0][0] == 1) {
+            j++;
+            last = 2;
+        } else {
+            i++;
+            last = 3;
+        }
+        switch (grid[0][0]) {
+            case 1:
+                j++;
+                last = 2;
+                break;
+            case 2:
+                i++;
+                last = 3;
+                break;
+            case 3:
+                i++;
+                last = 3;
+                break;
+            case 4:
+                grid[0][0] = 1;
+                if (hasValidPath(grid)) return true;
+                grid[0][0] = 2;
+                if (hasValidPath(grid)) return true;
+                return false;
+            case 5:
+                return false;
+            case 6:
+                j++;
+                last = 2;
+                break;
+        }
+        while (i < grid.length && j < grid[0].length) {
+            int key = grid[i][j];
+            int inx = 0;
+            if (Math.abs(dir[key][inx] - last) != 2) inx++;
+            if (inx == 1 && Math.abs(dir[key][inx] - last) != 2) return false;
+            if (i == grid.length - 1 && j == grid[0].length - 1) return true;
+            int next = inx ^ 1;
+            i += dirX[dir[key][next]];
+            j += dirY[dir[key][next]];
+            last = dir[key][inx ^ 1];
+        }
+        return false;
     }
 }
---
+++ src/math/PrimeFactors.java
@@ -0,0 +1,25 @@
+package math;
+
+/**
+ * 求正整数N(N>1)的质因数的个数。 相同的质因数需要重复计算。如120=2*2*2*3*5，共有5个质因数。
+ *
+ * @see <a href="https://www.nowcoder.com/questionTerminal/20426b85f7fc4ba8b0844cc04807fbd9">Here</a>
+ */
+public class PrimeFactors {
+    public static void main(String[] args) {
+        System.out.println(getPrimeFactors(120));
+    }
+
+    public static int getPrimeFactors(int n){
+        int count = 0;
+        for (int j = 2; j * j <= n; j++) {
+            while (n % j == 0) {
+                n /= j;
+//                System.out.println(n + " " + j);
+                count++;
+            }
+        }
+        if (n > 1) count++;
+        return count;
+    }
+}
