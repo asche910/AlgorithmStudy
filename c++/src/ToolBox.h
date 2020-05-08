553b54f629548a48f16c96a8a97d1ff4fc42d751
asche910 <asche910@163.com>
2020-05-08 11:56:54 +0800

    add Union-Find in c++

--- c++/src/ToolBox.h
+++ c++/src/ToolBox.h
@@ -31,4 +31,50 @@ void output(vector<T>& vec) {
     cout << endl;
 }
 
+
+
+/**
+ * Union-Find
+ */
+class UF {
+private:
+    int cnt;
+    vector<int> parent, size;
+public:
+    UF(int n) : cnt(n), parent(n), size(n, 1) {
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    void Union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
+        if (rootP == rootQ)
+            return;
+        if (size[rootP] > size[rootQ]) {
+            parent[rootQ] = rootP;
+            size[rootP] += size[rootQ];
+        }
+        else {
+            parent[rootP] = rootQ;
+            size[rootQ] += size[rootP];
+        }
+        cnt--;
+    }
+    bool connected(int p, int q) {
+        return find(p) == find(q);
+    }
+    int find(int x) {
+        while (parent[x] != x) {
+            parent[x] = parent[parent[x]];
+            x = parent[x];
+        }
+        return x;
+    }
+    int count() {
+        return cnt;
+    }
+};
+
 #endif
